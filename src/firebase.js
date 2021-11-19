46df2dcd31e5a51518f5abc60886d759415d3dea
yakuto15 <yakuto01058910@yahoo.co.jp>
2021-11-19 20:51:24 +0900

    ルーターセットアップ

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
-import firebase from "firebase";
-import "firebase/firestore";
+import firebase from "firebase"
+import "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAEhyByTPsob1LQiFt3ro3TTy0vjJxZK5w",
@@ -8,6 +8,6 @@ const firebaseConfig = {
   storageBucket: "ataoka-5c6eb.appspot.com",
   messagingSenderId: "422153424926",
   appId: "1:422153424926:web:223f850f6cdd06fe2a9e0f",
-};
+}
 
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
