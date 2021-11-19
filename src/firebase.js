efc61b97d8c7d6065b32a33e1565d365345b3749
igarashikoudainoMacBook-Pro <kodai.igarashi.2r@stu.hosei.ac.jp>
2021-11-19 20:21:13 +0900

    firebaseの設定をしました

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEhyByTPsob1LQiFt3ro3TTy0vjJxZK5w",
+  authDomain: "ataoka-5c6eb.firebaseapp.com",
+  projectId: "ataoka-5c6eb",
+  storageBucket: "ataoka-5c6eb.appspot.com",
+  messagingSenderId: "422153424926",
+  appId: "1:422153424926:web:223f850f6cdd06fe2a9e0f",
+};
+
+firebase.initializeApp(firebaseConfig);
