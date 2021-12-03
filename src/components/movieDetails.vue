7f9196b320805a5f1feffba1667f153f5ede7a97
S-Queen <sake4951@gmail.com>
2021-12-03 14:27:56 +0900

    一旦コミット

---
+++ src/components/movieDetails.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>映画のタイトル{{ movieTitle }}</h1>
+    <img src="" alt="" />
+    <h4>国：{{ country }} 主演:{{ actor }} 監督：{{ director }}</h4>
+    <p>ここに映画の説明入る</p>
+  </div>
+</template>
+
+<style>
+#app {
+  width: 20rem;
+  text-align: center;
+}
+</style>
