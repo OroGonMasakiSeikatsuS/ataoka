35359a20513adc74791bf886c63b82ccc4fcfe8d
arashi2000 <arashi.ogawa.2000@gmail.com>
2021-12-03 16:41:33 +0900

    変更確認

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <google-map />
+    <quiz />
   </div>
 </template>
 
 <script>
 import GoogleMap from "./components/GoogleMap.vue"
+import Quiz from "./components/quiz.vue"
 
 export default {
   name: "App",
   components: {
     GoogleMap,
+    Quiz,
   },
 }
 </script>
---
+++ src/components/quiz.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>クイズ</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLaughing: false,
+    }
+  },
+  methods: {
+    makeLaugh() {
+      this.isLaughing = true
+    },
+  },
+}
+</script>
